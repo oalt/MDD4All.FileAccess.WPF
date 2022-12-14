6266b6afe69bfc1bd8311d1cd98c52807b81ac39
Oliver Alt <oa@lacheweg21.de>
2022-12-14 15:02:44 +0100

    Initial code commit

---
+++ src/MDD4All.FileAccess.WPF/WpfFileSaver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using MDD4All.FileAccess.Contracts;
+using Microsoft.Win32;
+
+namespace MDD4All.FileAccess.WPF
+{
+    public class WpfFileSaver : IFileSaver
+    {
+        public bool ShowFileSaveDialog(out string selectedFilename, string defaultFielname = "", string defaultFileExtension = "", string filter = "All Files (*.*)|*.*")
+        {
+            bool result = false;
+            selectedFilename = null;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            if (!string.IsNullOrEmpty(filter))
+            {
+                saveFileDialog.Filter = filter;
+            }
+            if (!string.IsNullOrEmpty(defaultFielname))
+            {
+                saveFileDialog.FileName = defaultFielname;
+            }
+            if(string.IsNullOrEmpty(defaultFileExtension))
+            {
+                saveFileDialog.DefaultExt = defaultFileExtension;
+            }
+
+            bool? dialogResult = saveFileDialog.ShowDialog();
+
+            if(dialogResult.HasValue)
+            {
+                if(dialogResult.Value == true)
+                {
+                    selectedFilename = saveFileDialog.FileName;
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        public void WriteDataToFile(string filename, byte[] data)
+        {
+            try
+            {
+                File.WriteAllBytes(filename, data);
+            }
+            catch (Exception exception)
+            { }
+        }
+    }
+}
