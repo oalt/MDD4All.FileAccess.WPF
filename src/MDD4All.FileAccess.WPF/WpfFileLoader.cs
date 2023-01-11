b2a8e1f48c2395fe7e3cfb893e5468686c48b217
Oliver Alt <oa@lacheweg21.de>
2023-01-11 14:16:12 +0100

    Implementation of file loader

---
+++ src/MDD4All.FileAccess.WPF/WpfFileLoader.cs
@@ -0,0 +1,61 @@
+﻿using MDD4All.FileAccess.Contracts;
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace MDD4All.FileAccess.WPF
+{
+    public class WpfFileLoader : IFileLoader
+    {
+        public byte[] ReadDataFromFile(string filename)
+        {
+            byte[] result = null;
+
+            try
+            {
+                result = File.ReadAllBytes(filename);
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception);
+            }
+
+            return result;
+        }
+
+        public bool ShowOpenFileDialog(out string selectedFilename, string defaultFielname = "", string defaultFileExtension = "", string filter = "All Files (*.*)|*.*")
+        {
+            bool result = false;
+            selectedFilename = null;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (!string.IsNullOrEmpty(filter))
+            {
+                openFileDialog.Filter = filter;
+            }
+            if (!string.IsNullOrEmpty(defaultFielname))
+            {
+                openFileDialog.FileName = defaultFielname;
+            }
+            if (string.IsNullOrEmpty(defaultFileExtension))
+            {
+                openFileDialog.DefaultExt = defaultFileExtension;
+            }
+
+            bool? dialogResult = openFileDialog.ShowDialog();
+
+            if (dialogResult.HasValue)
+            {
+                if (dialogResult.Value == true)
+                {
+                    selectedFilename = openFileDialog.FileName;
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+    }
+}
